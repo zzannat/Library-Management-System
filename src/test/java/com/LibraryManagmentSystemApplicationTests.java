5245ba4634663d009df5a53b0a164d6d50605b02
zzannat <zzannat@my.centennialcollege.ca>
2024-04-14 13:51:22 -0400

    Added unit testcase and configured jacoco plugin

--- src/test/java/com/LibraryManagmentSystemApplicationTests.java
+++ src/test/java/com/LibraryManagmentSystemApplicationTests.java
@@ -1,13 +1,39 @@
 package com;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.model.Book;
+import com.repository.BookRepository;
+import com.service.BookServiceImpl;
+
+import reactor.core.publisher.Flux;
+
 
 @SpringBootTest
 class LibraryManagmentSystemApplicationTests {
+	@Mock
+    private BookRepository bookRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @InjectMocks
+    private BookServiceImpl bookService;
+
+    @Test
+    void testGetAll() {
+        // Mocking repository response
+        when(bookRepository.findAll()).thenReturn(Flux.just(new Book("Author", "Title", 10.0, 5)));
+
+        // Call the service method
+        Flux<Book> result = bookService.getAll();
+
+        // Assert the result
+        assertEquals(1, result.count().block());
+    }
+
 
 }
